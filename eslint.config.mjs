c1891702e3c86caedd03f7451e88d983cb4cb3d3
l-you <l-you@revotale.com>
2025-04-06 10:59:56 +0000

    Redesign linting becuae prettier is build in eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import love from 'eslint-config-love'
+import { globalIgnores } from 'eslint/config'
+
+export default [
+    globalIgnores('**/node_modules/**', '**/dist/**', '**/build/**'),
+  {
+    ...love,
+    files: ['**/*.js', '**/*.ts'],
+  },
+]
