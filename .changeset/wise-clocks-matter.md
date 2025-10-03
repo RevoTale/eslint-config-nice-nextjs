54ae036d0e66e804d3f315927f6e1a030ecfa015
l-you <l-you@revotale.com>
2025-10-03 19:37:57 +0000

    Rewrite from scratch eslint-config-love and eslint-plugin-next composition. Fix type and compatability errors. Reduce complexity of dependencies required for this eslint config.

---
+++ .changeset/wise-clocks-matter.md
@@ -0,0 +1,5 @@
+---
+"eslint-config-nice-nextjs": patch
+---
+
+Rewrite from scratch eslint-config-love and eslint-plugin-next composition. Fix type and compatability errors. Reduce complexity of dependencies required for this eslint config.
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-misused-spread -- workaround until tseslint or eslint-config-love will abandon their '.config.' funtion in favor of  eslint v9 defineConfig */
 
 import type  { ConfigArray } from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import love from 'eslint-config-love';
+import nextPlugin from '@next/eslint-plugin-next';
 //Temporary disable next.js plugin until it supports eslint v9
 const config: ConfigArray = defineConfig(
     // Global ignores
@@ -12,7 +14,19 @@ const config: ConfigArray = defineConfig(
             'node_modules/**/*'
         ],
     },
-    // @ts-expect-error -- workaround until tseslint or eslint-config-love will abandon their '.config.' funtion in favor of  eslint v9 defineConfig
+ {
+    plugins: {
+            // @ts-expect-error -- workaround until tseslint or eslint-config-love will abandon their '.config.' funtion in favor of  eslint v9 defineConfig
+      '@next/next': nextPlugin,
+    },
+  
+    rules: {
+                    // @ts-expect-error -- workaround until tseslint or eslint-config-love will abandon their '.config.' funtion in favor of  eslint v9 defineConfig
+        ...nextPlugin.configs.recommended.rules,
+                    // @ts-expect-error -- workaround until tseslint or eslint-config-love will abandon their '.config.' funtion in favor of  eslint v9 defineConfig
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
     {
         ...love,
        files: ['{src,app}/**/*.{js,ts,tsx}'],
